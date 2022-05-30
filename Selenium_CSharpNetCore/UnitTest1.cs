621908602b7a104612398b0394b18b7c91d61c86
Nicholai Dela Pena <nicholaipdp@gmail.com>
2022-05-30 15:20:22 +0800

    generate random details

--- Selenium_CSharpNetCore/UnitTest1.cs
+++ Selenium_CSharpNetCore/UnitTest1.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 
+
 namespace Selenium_CSharpNetCore
 {
     public class Tests : DriverHelper
@@ -21,6 +22,7 @@ namespace Selenium_CSharpNetCore
 
         public void Test1()
         {
+            
             var randomFname = RandomGenerator.generateFname();
             var randomLname = RandomGenerator.generateLname();
             var randomMname = RandomGenerator.generateMname();
@@ -29,8 +31,25 @@ namespace Selenium_CSharpNetCore
             var randomOccupations = RandomGenerator.generateOccupation();
             var randomIncome = RandomGenerator.generateIncome();
             var randomCountry = RandomGenerator.generateCountry();
-            //Driver.Navigate().GoToUrl("http://localhost/attendance");
-            //Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+            var randomCity = RandomGenerator.generateCity();
+            var randomProvince = RandomGenerator.generateProvince();
+            var randomStreet = RandomGenerator.generateStreet();
+            var randomBuildingNum = RandomGenerator.generateBuildingNum();
+            var randomSubdivision = RandomGenerator.generateSubdivision();
+            var randomPostalCode = RandomGenerator.generatePostalCode();
+            var randomDOB = RandomGenerator.generateDOB();
+            var randomDOB2 = RandomGenerator.generateDOB();
+            var randomCivilStatus = RandomGenerator.generateCivilStatus();
+            var randomCivilStatus2 = RandomGenerator.generateCivilStatus();
+            var randomFname2 = RandomGenerator.generateFname();
+            var randomLname2 = RandomGenerator.generateLname();
+            var randomMname2 = RandomGenerator.generateMname();
+            var randomSalutations2 = RandomGenerator.generateSalutation();
+            var randomPhonenumber2 = RandomGenerator.generatePhonenumber();
+            var randomOccupations2 = RandomGenerator.generateOccupation();
+            var randomIncome2 = RandomGenerator.generateIncome();
+
+           
 
             Driver.Navigate().GoToUrl("http://112.199.116.13:3001/login");
            
@@ -50,11 +69,11 @@ namespace Selenium_CSharpNetCore
 
             Driver.FindElement(By.Id("input_last_name")).SendKeys(randomLname);
 
-            Driver.FindElement(By.Id("input_birthdate")).SendKeys("30101995");
+            Driver.FindElement(By.Id("input_birthdate")).SendKeys(randomDOB);
 
             #region civilStatus
                 IWebElement civilStatus = Driver.FindElement(By.Id("input_civil_status"));
-                civilStatus.SendKeys("Married");
+                civilStatus.SendKeys(randomCivilStatus);
                 civilStatus.SendKeys(Keys.Down);
                 civilStatus.SendKeys(Keys.Enter);
             #endregion
@@ -93,7 +112,7 @@ namespace Selenium_CSharpNetCore
             IWebElement province = Driver.FindElement(By.Id("input_province"));
             province.Click();
             province.SendKeys(Keys.Control + "a" + Keys.Delete);
-            province.SendKeys("Argentina1");
+            province.SendKeys(randomProvince);
             #endregion
 
 
@@ -103,7 +122,7 @@ namespace Selenium_CSharpNetCore
             IWebElement city = Driver.FindElement(By.Id("input_city"));
             city.Click();
             city.SendKeys(Keys.Control + "a" + Keys.Delete);
-            city.SendKeys("Argentina2");
+            city.SendKeys(randomCity);
             #endregion
 
             #region subdivision
@@ -111,7 +130,7 @@ namespace Selenium_CSharpNetCore
             IWebElement subdivision = Driver.FindElement(By.Id("input_subdivision"));
             subdivision.Click();
             subdivision.SendKeys(Keys.Control + "a" + Keys.Delete);
-            subdivision.SendKeys("Olympic Village");
+            subdivision.SendKeys(randomSubdivision);
             #endregion
 
             #region street
@@ -119,51 +138,61 @@ namespace Selenium_CSharpNetCore
             IWebElement street = Driver.FindElement(By.Id("input_street"));
             street.Click();
             street.SendKeys(Keys.Control + "a" + Keys.Delete);
-            street.SendKeys("Fighter Street");
+            street.SendKeys(randomStreet);
             #endregion
 
             #region buildingNumber
             IWebElement buildingNumber = Driver.FindElement(By.Id("input_building_number"));
             buildingNumber.Click();
             buildingNumber.SendKeys(Keys.Control + "a" + Keys.Delete);
-            buildingNumber.SendKeys("12344");
+            buildingNumber.SendKeys(randomBuildingNum);
             #endregion
 
             #region postalCode
             IWebElement postalCode = Driver.FindElement(By.Id("input_postal_code"));
             postalCode.Click();
             postalCode.SendKeys(Keys.Control + "a" + Keys.Delete);
-            postalCode.SendKeys("2309");
+            postalCode.SendKeys(randomPostalCode);
             #endregion
 
             #region addCompanion
-            //Driver.FindElement(By.Id("button_add_companion")).Click();
-            //System.Threading.Thread.Sleep(5000);
-            //Driver.FindElement(By.Id("input_salutation")).SendKeys("Mr");
+            Driver.FindElement(By.Id("button_add_companion")).Click();
+            Driver.FindElement(By.Id("input_salutation")).SendKeys(randomSalutations2);
+            Driver.FindElement(By.Id("input_first_name")).SendKeys(randomFname2);
+            Driver.FindElement(By.Id("input_middle_name")).SendKeys(randomMname2);
 
-            //Driver.FindElement(By.Id("input_first_name")).SendKeys("Jowie");
+            Driver.FindElement(By.Id("input_last_name")).SendKeys(randomLname2);
 
-            //Driver.FindElement(By.Id("input_middle_name")).SendKeys("P");
+            Driver.FindElement(By.Id("input_birthdate")).SendKeys(randomDOB2);
 
-            //Driver.FindElement(By.Id("input_last_name")).SendKeys("Jurado");
+            #region civilStatus
+           
+            civilStatus.SendKeys(randomCivilStatus2);
+            civilStatus.SendKeys(Keys.Down);
+            civilStatus.SendKeys(Keys.Enter);
+            #endregion
 
-            //Driver.FindElement(By.Id("input_birthdate")).SendKeys("01101997");
+            Driver.FindElement(By.Id("input_contact_number")).SendKeys("09" + randomPhonenumber2);
+            Driver.FindElement(By.Id("input_email")).SendKeys(randomFname2 + randomLname2 + "@gmail.com");
 
-            //Driver.FindElement(By.Id("input_civil_status")).SendKeys("Married");
-            //Driver.FindElement(By.Id("input_civil_status")).SendKeys(Keys.Down);
-            //Driver.FindElement(By.Id("input_civil_status")).SendKeys(Keys.Enter);
+            #region gender
+           
+            gender.SendKeys("Male");
+            gender.SendKeys(Keys.Down);
+            gender.SendKeys(Keys.Enter);
+            #endregion
 
-            //Driver.FindElement(By.Id("input_gender")).SendKeys("Male");
-            //Driver.FindElement(By.Id("input_gender")).SendKeys(Keys.Down);
-            //Driver.FindElement(By.Id("input_gender")).SendKeys(Keys.Enter);
+            Driver.FindElement(By.Id("input_occupation")).SendKeys(randomOccupations2);
+            #region income
+            
+            income.SendKeys(randomIncome2);
+            income.SendKeys(Keys.Down);
+            income.SendKeys(Keys.Enter);
+            #endregion
 
-            //Driver.FindElement(By.Id("input_occupation")).SendKeys("Manager");
+            Driver.FindElement(By.Id("btn-retrieve-record")).Click();
 
-            //Driver.FindElement(By.Id("input_income")).SendKeys("Php 50,000 Above");
-            //Driver.FindElement(By.Id("input_income")).SendKeys(Keys.Down);
-            //Driver.FindElement(By.Id("input_income")).SendKeys(Keys.Enter);
-            //Driver.FindElement(By.Id("btn-retrieve-record")).Click();
-            //System.Threading.Thread.Sleep(5000);
+            System.Threading.Thread.Sleep(1000);
             #endregion
 
             Driver.FindElement(By.Id("button_next")).Click();
@@ -173,13 +202,14 @@ namespace Selenium_CSharpNetCore
             Driver.FindElement(By.ClassName("input_question_one_yes")).Click();
             Driver.FindElement(By.ClassName("input_question_two_no")).Click();
             Driver.FindElement(By.ClassName("input_question_three_yes")).Click();
+
+            Driver.FindElement(By.XPath("//*[@id='input_proceed_onboarding']/div/main/div/div/div[2]/div/div/div[2]/div[2]/form/div[5]/div/div/div/div[1]/div/div")).Click();
+
             System.Threading.Thread.Sleep(2000);
             Driver.FindElement(By.Id("button_submit")).Click();
 
             System.Threading.Thread.Sleep(500);
 
-            //*[@id="app"]/div[1]
-            //Driver.FindElement(By.XPath("//input[@id='app']/div[1]")).Click();
 
             IWebElement btnAction0 = Driver.FindElement(By.Id("btn-options-0"));
             btnAction0.Click();
